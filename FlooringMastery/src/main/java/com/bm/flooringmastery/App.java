c728bcb5821c3dad742f814ba61e299aec442cec
Benjamin Munoz <driver396@gmail.com>
2021-08-05 12:06:41 -0700

    Requirements elaborated, Some experimental code

--- FlooringMastery/src/main/java/com/bm/flooringmastery/App.java
+++ FlooringMastery/src/main/java/com/bm/flooringmastery/App.java
@@ -1,5 +1,11 @@
 package com.bm.flooringmastery;
 
+import java.io.IOException;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 /**
  * Acts as the entry point of the whole application
  *
@@ -8,7 +14,16 @@ package com.bm.flooringmastery;
  * date: Aug 5, 2021
  */
 public class App {
-    public static void main(String[] args) {
-        // do something
+    public static void main(String[] args) throws IOException {
+        FileSystem fs = FileSystems.getDefault();
+        
+        Path pp = FileSystems.getDefault().getPath("./Orders");
+        System.out.println(pp.toString());
+        
+        FileSystem fs2 = pp.getFileSystem();
+        
+        Files.walk(pp, 1).skip(1).forEach(path -> {
+            System.out.println(path.getFileName());
+        });
     }
 }
